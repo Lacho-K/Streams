185e53d43e66b96d111d56d01cc1c590c325f0fa
Lacho123 <44599728+Lacho123@users.noreply.github.com>
2021-08-03 14:36:29 +0300

    Add files via upload

---
+++ StudentStreams/StudentStreams/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+namespace StudentStreams
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, float> studentsInfo = new Dictionary<string, float>();
+
+            using (StreamReader reader = new StreamReader("students.txt"))
+            {
+                string line;
+                while((line = reader.ReadLine()) != null)
+                {
+                    string[] input = line.Split();
+                    studentsInfo.Add(input[0], float.Parse(input[1]));
+                }
+            }
+
+            using (StreamWriter writer = new StreamWriter("studentsSorted.txt"))
+            {
+                foreach (var student in studentsInfo.OrderBy(x => x.Key).ThenBy(x => x.Value))
+                {
+                    writer.WriteLine($"{student.Key} {student.Value}");
+                }
+            }            
+
+        }
+    }
+}
